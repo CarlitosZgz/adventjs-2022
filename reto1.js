85e852b8c1c0719eef18857282748aac8f32a0e4
Carlos Bernad Tena <119625372+CarlitosZgz@users.noreply.github.com>
2022-12-03 10:55:26 +0100

    Update reto1.js

--- reto1.js
+++ reto1.js
@@ -1,3 +1,13 @@
+const gifts = ['cat', 'game', 'socks']
+const wrapped = wrapping(gifts)
+
+console.log(wrapped)
+/* [
+  "*****\\n*cat*\\n*****",
+  "******\\n*game*\\n******",
+  "*******\\n*socks*\\n*******"
+] */
+
 function wrapping(g) {
         return (g.map (a => ("*".repeat(a.length+2))+"\n*"+a+"*\n"+"*".repeat(a.length+2)))
 }
